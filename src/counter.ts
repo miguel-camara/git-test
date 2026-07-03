0ec6cd82ba5571a7ebfee10434e507de8db26b3b
Miguel Ordonez Camara <miguelordonezcamara@hotmail.com>
2026-07-03 13:50:41 -0600

    Se un console.log a la rama main

--- src/counter.ts
+++ src/counter.ts
@@ -10,6 +10,8 @@ export function setupCounter(
   subtract.innerHTML = "";
   zero.innerHTML = "0";
 
+  console.log("Console.log main");
+
   label.innerHTML = `El contador es 0`;
   const setCounter = (count: number) => {
     counter = count;
